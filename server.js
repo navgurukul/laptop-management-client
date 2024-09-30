e2142699383cc05f157226e354582f544a857596
Shivansh Rawat <shivanshrawat587@gmail.com>
2024-09-30 12:24:48 +0530

    added commands

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on("connection", (ws) => {
   console.log("Client connected");
 
-  const commands = 
-    [
-      "sudo apt install apt-transport-https curl",
-      "sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg",
-      "echo 'deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main' | sudo tee /etc/apt/sources.list.d/brave-browser-release.list",
-      "sudo apt update",
-      "sudo apt install brave-browser"
-    ]
-  
+const commands = [
+  "sudo apt install -y curl",
+  "sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg",
+  "echo 'deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main' | sudo tee /etc/apt/sources.list.d/brave-browser-release.list",
+  "sudo apt update",
+  "sudo apt install -y brave-browser",
+];
   
   
 
